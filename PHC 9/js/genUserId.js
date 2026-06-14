b50100c1bfa0cda7f0c8039eacb1441a0bfe5ade
Minhazul Haque <MINHAZULHAQUE1987@GMAIL.COM>
2026-06-14 15:22:33 +0600

    Delete PHC 9 directory

--- PHC 9/js/genUserId.js
+++
@@ -1,7 +0,0 @@
-/* Generate user ID */
-function genUserId(name) {
-  const users = getUsers();
-  const prefix = 'BBS';
-  let num = users.filter(u=>u.role!=='admin').length + 1;
-  return prefix + String(num).padStart(4,'0');
-}
